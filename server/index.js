84855928bbf670b493d9a2c2453a726b39c2dfd7
Rishabhbaghel01 <rishabhbaghelfzd@gmail.com>
2026-06-24 22:54:46 +0530

    fix: update mongoose and strip w=majority from mongo URI

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ const io = require('socket.io')(server);
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = config.mongoURI.replace(/&?w=majority/i, '');
+console.log(mongoURI);
+
+const connect = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
-
-
 
 app.use(cors())
 
